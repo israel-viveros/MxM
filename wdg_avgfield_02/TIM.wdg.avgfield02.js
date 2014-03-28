97c021b84974d713b5de5430c04231f49e1d494a
Israel viveros <jose.viveros@esmas.net>
2014-03-27 20:27:13 -0600

    avances update wdg_avgfield_02

--- wdg_avgfield_02/TIM.wdg.avgfield02.js
+++ wdg_avgfield_02/TIM.wdg.avgfield02.js
@@ -10,6 +10,7 @@
 
         var wdgavgfieldObj = {
             urlFinal: 'http://static-televisadeportes.esmas.com/sportsdata/futbol/data/' + setting.idTorneo + '/' + setting.idMatch + '/match_summary.js',
+            urlmxmheader: 'http://static-televisadeportes.esmas.com/sportsdata/futbol/data/' + setting.idTorneo + '/' + setting.idMatch + '/match_header.js',
 
             loadMaster: function() {
                 $.ajax({
@@ -21,6 +22,23 @@
                 })
                     .done(function(data) {
                         wdgavgfieldObj.pintar(data);
+                    })
+                    .fail(function() {
+                        console.log("error al cargar el feed de summary actions");
+                    })
+            },
+            update: function() {
+                console.log("update");
+                $.ajax({
+                    url: wdgavgfieldObj.urlFinal,
+                    type: 'jsonp',
+                    dataType: 'jsonp',
+                    jsonpCallback: 'summaryActions',
+                    cache: false
+                })
+                    .done(function(data) {
+                        console.log("sucess data");
+                        //wdgavgfieldObj.pintar(data);
                     })
                     .fail(function() {
                         console.log("error al cargar el feed de summary actions");
@@ -109,8 +127,117 @@
                     $(this).css("display", "block");
                 });
 
+                wdgavgfieldObj.header();
 
             },
+            header: function() {
+                $.ajax({
+                    url: wdgavgfieldObj.urlmxmheader,
+                    jsonpCallback: 'mxmheader',
+                    type: 'GET',
+                    dataType: 'jsonp'
+                })
+                    .done(function(data) {
+                        console.log(data);
+                        wdgavgfieldObj.timeUpdate(data.fechaPartido, data.horaPartido);
+                    })
+                    .fail(function() {
+                        console.log("Error al cargar el header " + wdgavgfieldObj.urlmxmheader);
+                    })
+
+
+
+
+
+
+            },
+
+            timeUpdate: function(dia, hora) {
+                var tiempoActualizacion = 0;
+                var FechaPartido = dia.substring(3, 5) + '-' + dia.substring(0, 2) + '-' + dia.substring(8, 10) + ' ' + hora.substring(0, 5) + ':00';
+                $.ajax({
+                    url: "http://mxm.televisadeportes.esmas.com/deportes/home/timetvjsonp.js",
+                    async: false,
+                    cache: false,
+                    dataType: 'jsonp',
+                    jsonpCallback: 'timetv',
+                    success: function(data) {
+                        var arr = '';
+                        var m = 0;
+                        var anio = 0;
+
+                        horas = data.timetv;
+                        arr = data.fechatv.replace(/_/gi, "-").split("-");
+                        m = parseInt(arr[1]) + 1;
+
+                        if (String(m).length == 1) {
+                            m = '0' + m;
+                        }
+                        anio = parseInt(arr[2]) + 1900;
+                        fechas = m + '-' + arr[0] + '-' + anio;
+                        fechas = fechas + ' ' + horas + ':00';
+
+                        var a = new Date(FechaPartido);
+                        var b = new Date(fechas);
+
+                        var msDateA = Date.UTC(a.getFullYear(), a.getMonth() + 1, a.getDate());
+                        var msDateB = Date.UTC(b.getFullYear(), b.getMonth() + 1, b.getDate());
+
+                        if (parseFloat(msDateA) < parseFloat(msDateB)) {
+                            console.log("MENOR");
+                        } else {
+                            if (parseFloat(msDateA) == parseFloat(msDateB)) {
+                                //console.log("IGUAL");
+                                tiempoActualizacion = 60000;
+                                var resta = parseInt(b.getHours() - a.getHours());
+                                //cop
+                                if (b.getHours() >= a.getHours()) {
+                                    //console.log("ya empezo el partido");
+                                    //Ya empezo el partido, actualizar valores cada minuto                                      
+                                    tiempoActualizacion = 60000;
+                                } else {
+                                    var h1 = a.getHours();
+                                    var h2 = b.getHours();
+                                    var m1 = a.getMinutes();
+                                    var m2 = b.getMinutes();
+                                    //Validar cuantos minutos faltan para el inicio del partido
+                                    var minutosrestantes = (((h1 - h2) * 60) + m1) - m2;
+
+                                    if (minutosrestantes <= 15) {
+                                        //console.log("faltan menos de 15 min");
+                                        //Faltan 15 minutos o menos para el inicio, actualizar los valores cada minuto
+                                        tiempoActualizacion = 60000;
+
+                                    } else {
+                                        //console.log("faltan mas de 15 pero menos de 1hr " + minutosrestantes);
+                                        //Faltan mas de 15 minutos para el inicio, actualizar los valores cada 15 minutos pero menos de una hora
+
+                                        (minutosrestantes < 60) ? tiempoActualizacion = 900000 : '';
+                                    }
+                                }
+                                //cop
+                                //console.log(tiempoActualizacion)
+                                if (tiempoActualizacion !== 0) {
+                                    setInterval(function() {
+                                        wdgavgfieldObj.update()
+                                    }, tiempoActualizacion);
+                                }
+                                //setInterval(function(){wdg_smex_strategy.updatePlayers()},15000);
+
+                            } else {
+                                if (parseFloat(msDateA) > parseFloat(msDateB)) {
+                                    console.log("MAYOR");
+                                } else {
+                                    //console.log("Error no actualizo");
+                                }
+                            }
+                        }
+                        setInterval(function() {
+                            wdgavgfieldObj.update()
+                        }, 6000);
+                    }
+                });
+            }, // End timeUpdate()
 
 
 
