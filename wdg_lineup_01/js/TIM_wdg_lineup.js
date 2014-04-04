a84d9ce90d6e0b2531d1f46a7e70867d032479a8
Israel viveros <jose.viveros@esmas.net>
2014-04-03 19:57:47 -0600

    feeds de prueba para los drops en lineup

--- wdg_lineup_01/js/TIM_wdg_lineup.js
+++ wdg_lineup_01/js/TIM_wdg_lineup.js
@@ -11,8 +11,10 @@
         var globalthis = this;
 
         var wdgLineUpOb = {
-            feedDropLocal: 'http://lab.israelviveros.com/deportes/wdg_lineup_01/' + settings.idTorneo + '/' + settings.idEquipo + '/matchesclub.js',
-            feedDropVisit: 'http://lab.israelviveros.com/deportes/wdg_lineup_01/' + settings.idTorneo + '/' + settings.idEquipo2 + '/matchesclub.js',
+            feedDropLocal: 'http://static-televisadeportes.esmas.com/sportsdata/futbol/data/356/clubes/4/matchesclub.js',
+            //feedDropLocal: 'http://lab.israelviveros.com/deportes/wdg_lineup_01/' + settings.idTorneo + '/' + settings.idEquipo + '/matchesclub.js',
+            //feedDropVisit: 'http://lab.israelviveros.com/deportes/wdg_lineup_01/' + settings.idTorneo + '/' + settings.idEquipo2 + '/matchesclub.js',
+            feedDropVisit: 'http://static-televisadeportes.esmas.com/sportsdata/futbol/data/356/clubes/4/matchesclub.js',
 
             inicio: function() {
                 var maquetado = "";
@@ -238,13 +240,13 @@
                     url: feed,
                     type: 'GET ',
                     dataType: 'jsonp ',
-                    jsonpCallback: 'matches'
+                    jsonpCallback: 'effectivenessByTeam'
                 })
                     .done(function(data) {
-                        //console.log(data);
+                        console.log(data);
                         var tmp = "";
-                        for (var i = 0; i < data.Team.length; i++) {
-                            tmp += '<li><p data-matchid="' + data.Team[i].matchid + '">' + data.Team[i].week + '</p></li>';
+                        for (var i = 0; i < data.efectividad.length; i++) {
+                            tmp += '<li><p data-matchid="' + data.efectividad[i].matchid + '">' + data.efectividad[i].weekName + '</p></li>';
                         };
 
                         $(ID).html(tmp);
