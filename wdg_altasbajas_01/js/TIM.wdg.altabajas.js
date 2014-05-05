87ce210a8dfa2f73edcac17c7518b8ed9c4aaf4b
Israel viveros <jose.viveros@esmas.net>
2014-05-05 17:21:15 -0500

    termino del wdg_altasbajas_01 (draft)

--- wdg_altasbajas_01/js/TIM.wdg.altabajas.js
+++ wdg_altasbajas_01/js/TIM.wdg.altabajas.js
@@ -7,7 +7,9 @@
 (function() {
     $.fn.wdgAltasbajas = function(parametros) {
         var setting = $.extend({
-            'idTournament': 0
+            'idTournament': 0,
+            'abbody': false,
+            'title': ''
         }, parametros);
 
 
@@ -17,6 +19,7 @@
         var objAltasbajas = {
             feedEquipos: 'http://static-televisadeportes.esmas.com/sportsdata/futbol/data/' + setting.idTournament + '/teamsclassification.js',
             feedDraft: 'http://lab.israelviveros.com/draft/' + setting.idTournament + '/draft.js',
+            secondIDdraw: $("#altasbajasbodyTIM"),
 
             iniciaMaquetado: function() {
                 var maquetado = '<div class="wdg_altasbajas_01 background-color1">';
@@ -112,8 +115,8 @@
                                 maquetado += '</a>';
                                 maquetado += '</td>';
 
-                                altas += '<td id="al' + data.dataEstadistica[i].idTeam + '">-</td>';
-                                bajas += '<td id="ba' + data.dataEstadistica[i].idTeam + '">-</td>';
+                                altas += '<td class="al' + data.dataEstadistica[i].idTeam + '">-</td>';
+                                bajas += '<td class="ba' + data.dataEstadistica[i].idTeam + '">-</td>';
                             }
                         };
                         $("#firstlogo").html(maquetado);
@@ -121,7 +124,12 @@
                         $("#bajTIM").html(bajas);
 
                         try {
-                            objAltasbajas.infodraft();
+                            if (setting.abbody === true) {
+                                objAltasbajas.secondDraw(data);
+                            } else {
+                                objAltasbajas.infodraft();
+                            }
+
                         } catch (e) {
                             console.log(e);
                         }
@@ -137,7 +145,8 @@
             },
 
             infodraft: function() {
-                var maquetado = "";
+                var maquetado = "",
+                    total = 0;
 
                 $.ajax({
                     url: objAltasbajas.feedDraft,
@@ -152,13 +161,14 @@
                             target = finalYea + " " + data.draftEnd;
 
                         for (var i = 0; i < data.draftTeams.length; i++) {
-                            if (i < 18) {
-                                altas = data.draftTeams[i].teamAdd;
-                                bajas = data.draftTeams[i].teamDelete;
+                            altas = data.draftTeams[i].teamAdd;
+                            bajas = data.draftTeams[i].teamDelete;
 
-                                $("#al" + data.draftTeams[i].id).text(altas);
-                                $("#ba" + data.draftTeams[i].id).text(bajas);
-                            }
+                            $(".al" + data.draftTeams[i].id).text(altas);
+                            $(".ba" + data.draftTeams[i].id).text(bajas);
+
+                            total = parseInt(altas) + parseInt(bajas);
+                            $(".tot" + data.draftTeams[i].id).text(total);
                         };
 
                         (typeof(data.draftTotalAdd) === "string") ? $("#totAl").text(data.draftTotalAdd) : '';
@@ -182,18 +192,28 @@
 
 
             },
+            spanish: function() {
+                $.countdown.regionalOptions['es'] = {
+                    labels: ['A\u00F1os', 'Meses', 'Semanas', 'D\u00EDas', 'Hrs.', 'Min.', 'Seg.'],
+                    labels1: ['A\u00F1o', 'Mes', 'Semana', 'D\u00EDas', 'Hrs.', 'Min.', 'Seg.'],
+                    compactLabels: ['a', 'm', 's', 'g'],
+                    whichLabels: null,
+                    digits: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+                    timeSeparator: ':',
+                    isRTL: false
+                };
+                $.countdown.setDefaults($.countdown.regionalOptions['es']);
+            },
 
 
             startCountdown: function(target) {
+                objAltasbajas.spanish();
                 //new Date(year, month, day, hours, minutes, seconds, milliseconds)
-                function hell() {
-                    console.log("hey")
-                }
+
                 var austDay = new Date(target)
                 $('#countdownDraft').countdown({
                     until: austDay,
-                    format: 'odHM',
-                    onTick: hell,
+                    format: 'ODHMS',
                     layout: '<td rowspan="2" class="textcolor-title2 cut"><div class="vline"></div>El torneo comienza en:</td>' +
                         '<td class="textcolor-title3" id="months">{on}</td>' +
                         '<td class="textcolor-title3">·</td>' +
@@ -203,11 +223,59 @@
                         '<td class="textcolor-title3">:</td>' +
                         '<td class="textcolor-title3" id="minutes">{mn}</td>' +
                         '<td class="textcolor-title3">:</td>' +
-                        '{s<}<td class="textcolor-title3" id="seconds">{sn}</td>{s>}'
+                        '<td class="textcolor-title3" id="seconds">{sn}</td>'
                 });
 
 
+
+
+
             },
+
+            secondDraw: function(data) {
+                console.log(data)
+                var maquetado = "";
+                maquetado += '<div class="wdg_stats_teams_02" data-enhance="false">    ';
+                maquetado += '<h2 class="textcolor-title2">' + setting.title + '</h2>';
+                maquetado += '<div class="more_teams_stadistics">';
+                maquetado += '<div class="teams_stadistics_table">';
+
+                for (var i = 0; i < data.dataEstadistica.length; i++) {
+
+                    maquetado += '<div class="teams_stadistics">';
+                    maquetado += '<a class="link_table" target="_self" href="' + data.dataEstadistica[i].webNameTeam + '.html">';
+                    maquetado += '<div class="name_team"><h3>' + data.dataEstadistica[i].nameTeam + '</h3></div>';
+                    maquetado += '<div class="img_team"><img src="' + data.dataEstadistica[i].urlLogoClub + '" alt="' + data.dataEstadistica[i].nameTeam + '"></div>';
+                    maquetado += '<div class="textcolor-title4 tot' + data.dataEstadistica[i].idTeam + '">-</div>';
+                    maquetado += '<div class="titles_left">';
+                    maquetado += '<p class="textcolor-title1">ALTAS</p>';
+                    maquetado += '<p class="textcolor-title2 al' + data.dataEstadistica[i].idTeam + '">-</p>';
+                    maquetado += '</div>';
+                    maquetado += '<div class="separator_title"></div>';
+                    maquetado += '<div class="titles_right">';
+                    maquetado += '<p class="textcolor-title1">BAJAS</p>';
+                    maquetado += '<p class="textcolor-title2 ba' + data.dataEstadistica[i].idTeam + '">-</p>';
+                    maquetado += '</div>';
+                    maquetado += '<div class="marrow"><i class="tvsa-caret-right"></i></div>';
+                    maquetado += '</a>';
+                    maquetado += '</div>';
+                    if (i !== 3 && i !== 7 && i !== 11 && i !== 15 && i !== 19) {
+                        maquetado += '<div class="separator_team"></div>';
+                    }
+
+                };
+
+                maquetado += '</div>';
+                maquetado += '</div>';
+                maquetado += '<div class="degraded"></div ></div>';
+
+
+
+                $("#altasbajasbodyTIM").html(maquetado);
+
+                objAltasbajas.infodraft();
+
+            }
 
 
         };
