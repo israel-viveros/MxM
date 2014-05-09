2236fda6424073163d54f71592212d3d9f9253e8
Israel viveros <jose.viveros@esmas.net>
2014-05-09 12:43:39 -0500

    fix jornadas drop no cargaba en cq

--- wdg_altasbajas_result_01/TIM_wdg_altasbajas.js
+++ wdg_altasbajas_result_01/TIM_wdg_altasbajas.js
@@ -1,6 +1,6 @@
 /*!
  *   TIM Developer: Israel Viveros
- *   Version: 2.3.0
+ *   Version: 2.3.1
  *   Copyright: Televisa Interactive Media (2014)
  */
 ;
@@ -576,138 +576,141 @@
 ;
 jQuery(function($) {
     (function($, T) {
-        /*Swipe*/
-        var altWdgResult01 = 0;
-        if ($(window).width() < 978 && $(window).width() > 624) {
-            altWdgResult01 = 568;
-        } else {
-            altWdgResult01 = 163;
-        }
 
-        $('.wdg_altasbajas_result_01 .deg').bind('swipeup', function() {
-            $(this).animate({
-                'scrollTop': $(this).scrollTop() + altWdgResult01
-            }, 500);
-        });
-        $('.wdg_altasbajas_result_01 .deg').bind('swipedown', function() {
-            $(this).animate({
-                'scrollTop': $(this).scrollTop() - altWdgResult01
-            }, 500);
-        });
+        setTimeout(function() {
+            /*Swipe*/
+            var altWdgResult01 = 0;
+            if ($(window).width() < 978 && $(window).width() > 624) {
+                altWdgResult01 = 568;
+            } else {
+                altWdgResult01 = 163;
+            }
 
-
-        $('.wdg_altasbajas_result_01').each(function(ix, element) {
-            var $this = $(this),
-                Pointer = {
-                    UP: (T.getIsTouchDevice()) ? 'touchend' : 'mouseup',
-                    DOWN: (T.getIsTouchDevice()) ? 'touchstart' : 'mousedown'
-                },
-                $theUl = $this.find('>ul');
-
-            $this.find('a.prev, a.next, .deportes-prev, .deportes-next').click(function(event) {
-                event.preventDefault();
+            $('.wdg_altasbajas_result_01 .deg').bind('swipeup', function() {
+                $(this).animate({
+                    'scrollTop': $(this).scrollTop() + altWdgResult01
+                }, 500);
             });
-
-            $this.find('a.prev, .deportes-prev').bind(Pointer.DOWN, function() {
-                $theUl.animate({
-                    'scrollTop': $theUl.scrollTop() - $theUl.height() - 7
+            $('.wdg_altasbajas_result_01 .deg').bind('swipedown', function() {
+                $(this).animate({
+                    'scrollTop': $(this).scrollTop() - altWdgResult01
                 }, 500);
             });
 
-            $this.find('a.next, .deportes-next').bind(Pointer.DOWN, function() {
-                $theUl.animate({
-                    'scrollTop': $theUl.scrollTop() + $theUl.height() + 7
-                }, 500);
-            });
-        });
 
+            $('.wdg_altasbajas_result_01').each(function(ix, element) {
+                var $this = $(this),
+                    Pointer = {
+                        UP: (T.getIsTouchDevice()) ? 'touchend' : 'mouseup',
+                        DOWN: (T.getIsTouchDevice()) ? 'touchstart' : 'mousedown'
+                    },
+                    $theUl = $this.find('>ul');
 
-        $list = $('.wdg_altasbajas_result_01 .deg').size();
-        $altura_li = parseInt($('.wdg_altasbajas_result_01 .deg').height());
-        $altura = ($altura_li * $list) + $list;
-
-        var $parent = $('.wdg_altasbajas_result_01 ');
-        var $dropdownAnchor = $parent.find('.lineaResultado .filter');
-        $dropdownAnchor.on('click', function(evt) {
-            var $listItems = $(this).find('.wdg_altasbajas_result_012_dropdownlist');
-            var $visibility = $listItems.css('visibility');
-            var padre = $(this);
-            if ($visibility == 'hidden')
-                $listItems.css({
-                    visibility: 'visible',
-                    height: 'auto',
-                    'max-height': '156px',
-                    'overflow-y': 'scroll',
-                    'overflow-x': 'hidden'
+                $this.find('a.prev, a.next, .deportes-prev, .deportes-next').click(function(event) {
+                    event.preventDefault();
                 });
-            else
-                $listItems.css({
-                    visibility: 'hidden',
-                    height: '0px'
+
+                $this.find('a.prev, .deportes-prev').bind(Pointer.DOWN, function() {
+                    $theUl.animate({
+                        'scrollTop': $theUl.scrollTop() - $theUl.height() - 7
+                    }, 500);
                 });
-            var $dropdownItems2 = $(this).find('.wdg_altasbajas_result_012_dropdownlist li');
-            $dropdownItems2.bind('click', function(evt) {
-                evt.preventDefault();
-                padre.find('.wdg_altasbajas_result_012_dropdowncontent p').html($(this).find('p').html());
+
+                $this.find('a.next, .deportes-next').bind(Pointer.DOWN, function() {
+                    $theUl.animate({
+                        'scrollTop': $theUl.scrollTop() + $theUl.height() + 7
+                    }, 500);
+                });
             });
 
-            $listItems.bind('mouseleave', function(evt) {
-                evt.preventDefault();
-                var visibilidad = $(this).css('visibility');
-                if (visibilidad == 'visible') {
-                    $(this).css({
+
+            $list = $('.wdg_altasbajas_result_01 .deg').size();
+            $altura_li = parseInt($('.wdg_altasbajas_result_01 .deg').height());
+            $altura = ($altura_li * $list) + $list;
+
+            var $parent = $('.wdg_altasbajas_result_01 ');
+            var $dropdownAnchor = $parent.find('.lineaResultado .filter');
+            $dropdownAnchor.on('click', function(evt) {
+                console.log("drop")
+                var $listItems = $(this).find('.wdg_altasbajas_result_012_dropdownlist');
+                var $visibility = $listItems.css('visibility');
+                var padre = $(this);
+                if ($visibility == 'hidden')
+                    $listItems.css({
+                        visibility: 'visible',
+                        height: 'auto',
+                        'max-height': '156px',
+                        'overflow-y': 'scroll',
+                        'overflow-x': 'hidden'
+                    });
+                else
+                    $listItems.css({
                         visibility: 'hidden',
                         height: '0px'
                     });
-                }
+                var $dropdownItems2 = $(this).find('.wdg_altasbajas_result_012_dropdownlist li');
+                $dropdownItems2.bind('click', function(evt) {
+                    evt.preventDefault();
+                    padre.find('.wdg_altasbajas_result_012_dropdowncontent p').html($(this).find('p').html());
+                });
+
+                $listItems.bind('mouseleave', function(evt) {
+                    evt.preventDefault();
+                    var visibilidad = $(this).css('visibility');
+                    if (visibilidad == 'visible') {
+                        $(this).css({
+                            visibility: 'hidden',
+                            height: '0px'
+                        });
+                    }
+                });
             });
-        });
 
-        /*Monitoreo scroll*/
-        $('.wdg_altasbajas_result_01 .deg').scroll(function() {
-            var $war1_position = $(this).scrollTop();
+            /*Monitoreo scroll*/
+            $('.wdg_altasbajas_result_01 .deg').scroll(function() {
+                var $war1_position = $(this).scrollTop();
 
-            //alert($(this).height());
-            if ($(window).width() < 624) {
-                $war1_altura = 813;
-            }
-            if ($.browser.msie) {
-                $war1_altura = 470;
-            } else {
-                $war1_altura = $(this).height()
-            }
-            var $hijoAltura = $('.wdg_altasbajas_result_01 .deg').children('li').eq(0).outerHeight();
-            var $multi = $war1_position + 1;
-            var $newAlt = $multi * $hijoAltura;
-
-
-            if ($newAlt >= $war1_altura) {
-                $(this).siblings('.degraded').css("visibility", "hidden");
-                //$(this).siblings('.controls').children('.next').children('.tvsa-caret-down').css('color','#000');
-
-                $(this).siblings('.controls').children().children('.tvsa-caret-down').parent().removeClass('bgactive');
-                $(this).siblings('.controls').children().children('.tvsa-caret-down').parent().addClass('bginactive');
-
-            } else {
-                $(this).siblings('.degraded').css("visibility", "visible");
-                //$(this).siblings('.controls').children().children('.tvsa-caret-down').css('color','#FFF');
-                $(this).siblings('.controls').children().children('.tvsa-caret-down').parent().removeClass('bginactive');
-                $(this).siblings('.controls').children().children('.tvsa-caret-down').parent().addClass('bgactive');
-            }
-
-            if ($war1_position == 0) {
-                //$(this).siblings('.controls').children('.prev').children('.tvsa-caret-up').css('color','#000');
-                $(this).siblings('.controls').children().children('.tvsa-caret-up').parent().removeClass('bgactive');
-                $(this).siblings('.controls').children().children('.tvsa-caret-up').parent().addClass('bginactive');
-            } else {
-                //$(this).siblings('.controls').children('.prev').children('.tvsa-caret-up').css('color','#FFF');
-                $(this).siblings('.controls').children().children('.tvsa-caret-up').parent().removeClass('bginactive');
-                $(this).siblings('.controls').children().children('.tvsa-caret-up').parent().addClass('bgactive');
-            }
-            //console.log('Scroll: '+$war1_position+' suma: '+$(this).height());
-        });
+                //alert($(this).height());
+                if ($(window).width() < 624) {
+                    $war1_altura = 813;
+                }
+                if ($.browser.msie) {
+                    $war1_altura = 470;
+                } else {
+                    $war1_altura = $(this).height()
+                }
+                var $hijoAltura = $('.wdg_altasbajas_result_01 .deg').children('li').eq(0).outerHeight();
+                var $multi = $war1_position + 1;
+                var $newAlt = $multi * $hijoAltura;
 
 
+                if ($newAlt >= $war1_altura) {
+                    $(this).siblings('.degraded').css("visibility", "hidden");
+                    //$(this).siblings('.controls').children('.next').children('.tvsa-caret-down').css('color','#000');
+
+                    $(this).siblings('.controls').children().children('.tvsa-caret-down').parent().removeClass('bgactive');
+                    $(this).siblings('.controls').children().children('.tvsa-caret-down').parent().addClass('bginactive');
+
+                } else {
+                    $(this).siblings('.degraded').css("visibility", "visible");
+                    //$(this).siblings('.controls').children().children('.tvsa-caret-down').css('color','#FFF');
+                    $(this).siblings('.controls').children().children('.tvsa-caret-down').parent().removeClass('bginactive');
+                    $(this).siblings('.controls').children().children('.tvsa-caret-down').parent().addClass('bgactive');
+                }
+
+                if ($war1_position == 0) {
+                    //$(this).siblings('.controls').children('.prev').children('.tvsa-caret-up').css('color','#000');
+                    $(this).siblings('.controls').children().children('.tvsa-caret-up').parent().removeClass('bgactive');
+                    $(this).siblings('.controls').children().children('.tvsa-caret-up').parent().addClass('bginactive');
+                } else {
+                    //$(this).siblings('.controls').children('.prev').children('.tvsa-caret-up').css('color','#FFF');
+                    $(this).siblings('.controls').children().children('.tvsa-caret-up').parent().removeClass('bginactive');
+                    $(this).siblings('.controls').children().children('.tvsa-caret-up').parent().addClass('bgactive');
+                }
+                //console.log('Scroll: '+$war1_position+' suma: '+$(this).height());
+            });
+
+        }, 1500);
     })($, Televisa);
 });
 
